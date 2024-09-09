d041c303a5dd0b6a16715ac271437cc773bc4974
iTsDante07 <84541745+iTsDante07@users.noreply.github.com>
2024-09-09 10:58:34 +0200

    Add files via upload

---
+++ pra/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Undefined sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.MakeSound(); 
+        myCat.MakeSound();  
+    }
+}
